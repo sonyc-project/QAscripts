3a47d1ba25e3877802856a0731b577423cfbd2ea
Annie Chu <chuannie4@gmail.com>
2022-07-21 13:02:03 -0400

    adding functionality on sensor health

--- ctlscraper.py
+++ ctlscraper.py
@@ -19,15 +19,46 @@ api.login()
 
 """
 || checking status of sensor 
-    - is mic connected?
+    - is mic connected? DONE
     - is cell hat connected? (is there ppp0/cell_connected=True?)
     - is there an IP address?
     - is signal strength good? (above 85%?)
-    - are disk levels good? (tmp_usage, root_usage, varlog_usage)
+    - are disk levels good? (tmp_usage, root_usage, varlog_usage) DONE
+    - is laeq within range? (~50-60dB) DONE
 
 """
-status = api.status(deploy_id, start='now-5s')
-# pp.pprint(status)
+def sensorCheck(deployment_id):
+    status = api.status(deployment_id, start='now-30s')
+    s = status[-1]
+    # print(s)
+
+    #--CHECK DISK LEVELS:
+    diskVariables = ['tmp_usage','root_usage','varlog_usage']
+    diskLevels = {key: s[key] for key in s.keys() if key in diskVariables}
+    # print(diskLevels)
+    outBounds = [i for i in diskLevels if diskLevels[i] >=15]
+    # print(outBounds)
+    if not outBounds:
+        print('GOOD || Disk Levels: in bounds')
+        for g in diskLevels:
+            pp.pprint(f'{g}:{diskLevels[g]}')
+    else:
+        for x in outBounds:
+            print(f'Error: {x} value out of bounds - {diskLevels[x]}')
+
+    #--CHECK MIC CONNECTIVITY:
+    if s['mic_connected'] == 1:
+        print('GOOD || Mic: connected')
+    else:
+        print(f"mic bad {s['mic_connected']}")
+
+    #--CHECK LAEQ LEVELS:
+    if s['laeq'] > 45 and s['laeq'] < 60:
+        print(f"GOOD || LAeq levels -  {s['laeq']}")
+    else:
+        print(f"Error: laeq levels out of bounds - {s['laeq']}")
+
+sensorCheck(deploy_id)
 
 """
 || checking SPL levels specifically
@@ -41,13 +72,13 @@ spl = api.spl(deploy_id,start = 'now-1m')
     - is there a title?
     - is life_stage active?
 """
-def check_deployment():
-    dd = api.deploy.info(deploy_id) #deployment data
-    targets = ['title', 'deployment_id', 'life_stage', 'fqdn', 'description', 'floor'] #can get rid of fqdn once fqdn replaces deployment_id
+def deploymentCheck(deployment_id):
+    dd = api.deploy.info(deployment_id) #deployment data
+    targets = ['title', 'deployment_id', 'life_stage', 'fqdn'] #can get rid of fqdn once fqdn replaces deployment_id
     result = {key: dd[key] for key in dd.keys() if key in targets}
     # pp.pprint(result)
     noValues = [i for i in result if not result[i]]
-    print(noValues)
+    # print(noValues)
     if not noValues:
         print('Success: Deployment info looks good')
     else:
@@ -55,22 +86,7 @@ def check_deployment():
             print(f'ERROR: no value for - {error}')
             # raise KeyError(f'ERROR: no value for - {error}')
 
-check_deployment()
-
-# for item in result:
-#     if not result[item]:
-#         raise KeyError(f'ERROR: no value for - {item}')
-#     else:
-#         print('Deployment looks good! ')
-
-# noValues = []
-# for item in deployData:
-#     if not deployData[item]:
-#         noValues.append(item)
-#     # print(f'{item}|| {x[item]}')
-#     # print(data.key, data.values)
-# pp.pprint(f'OK: no value for {noValues}!')
-
+# deploymentCheck(deploy_id)
 
 """
 || check audio data
