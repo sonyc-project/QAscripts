a4bdd65c016ad1c77d6bb73a66c225c8593fb19e
Annie Chu <chuannie4@gmail.com>
2022-07-28 10:55:51 -0400

    removed nonessential code

--- ctlscraper.py
+++ ctlscraper.py
@@ -30,13 +30,6 @@ api.login()
     - [DONE] is laeq within range? (~50-60dB)
 
 """
-# def pullData(deployment_id):
-#     sensorStatus = api.status(deployment_id, start='now-10s') #sensor data
-#     deploymentStatus = api.deploy.info(deployment_id) #deployment data
-#     pp.pprint(sensorStatus)
-#     print('---')
-#     pp.pprint(deploymentStatus)
-#     return sensorStatus, deploymentStatus
 
 class SensorHealth:
     def __init__(self, deploymentID):
@@ -107,60 +100,6 @@ class SensorHealth:
 
 
 """
-def sensorCheck(deployment_id):
-    status = api.status(deployment_id, start='now-30s')
-    s = status[-1]
-    # pp.pprint(s)
-
-    #-- CHECK CELL CONNECTION:
-    if s['cell_connected'] and s['ppp0_ip']:
-        print(f"GOOD || Cell Hat USB connected & online")
-    elif s['cell_connected'] and not s['ppp0_ip']:
-        print(f"Error: Cell Hat USB connection detected, but no cell internet connection, is there a sim card?")
-    else:
-        print(f"Error: Cell Hat USB not connected")
-
-    #-- CHECK CELL_STRENGTH:
-    if np.abs(s['cell_strength']) > 85:
-        print(f"GOOD || CELL STRENGTH: {np.abs(s['cell_strength'])}%")
-    else:
-        print(f"Error: Bad cell strength {np.abs(s['cell_strength'])}%")
-
-    #-- CHECK IP ADDRESS:
-    if s['wlan0_ip']:
-        print(f"GOOD || IP ADDRESS: {s['wlan0_ip']}")
-    else:
-        print(f"Error: No IP ADDRESS")
-
-    #-- CHECK DISK LEVELS:
-    diskVariables = ['tmp_usage','root_usage','varlog_usage']
-    diskLevels = {key: s[key] for key in s.keys() if key in diskVariables}
-    # print(diskLevels)
-    outBounds = [i for i in diskLevels if diskLevels[i] >=15]
-    # print(outBounds)
-    if not outBounds:
-        print('GOOD || Disk Levels: in bounds')
-        for g in diskLevels:
-            pp.pprint(f'{g}:{diskLevels[g]}')
-    else:
-        for x in outBounds:
-            print(f'Error: {x} value out of bounds - {diskLevels[x]}')
-
-    #-- CHECK MIC CONNECTIVITY:
-    if s['mic_connected'] == 1:
-        print('GOOD || Mic: connected')
-    else:
-        print(f"mic bad {s['mic_connected']}")
-
-    #-- CHECK LAEQ LEVELS:
-    if s['laeq'] > 40 and s['laeq'] < 60:
-        print(f"GOOD || LAeq levels -  {s['laeq']}")
-    else:
-        print(f"Error: laeq levels out of bounds - {s['laeq']}")
-
-# sensorCheck(deploy_id)
-"""
-"""
 || [DONE] checking deployment info
     - is there a title?
     - is life_stage active?
@@ -183,23 +122,6 @@ class DeploymentHealth:
 
 testid_deploy = DeploymentHealth(deploy_id) 
 testid_deploy.checkValues()
-"""           
-def deploymentCheck(deployment_id):
-    dd = api.deploy.info(deployment_id) #deployment data
-    targets = ['title', 'deployment_id', 'life_stage', 'fqdn'] #can get rid of fqdn once fqdn replaces deployment_id
-    result = {key: dd[key] for key in dd.keys() if key in targets}
-    # pp.pprint(result)
-    noValues = [i for i in result if not result[i]]
-    # print(noValues)
-    if not noValues:
-        print('Success: Deployment info looks good')
-    else:
-        for error in noValues:
-            print(f'ERROR: no value for - {error}')
-            # raise KeyError(f'ERROR: no value for - {error}')
-
-# deploymentCheck(deploy_id)
-"""
 
 """
 || check audio data
