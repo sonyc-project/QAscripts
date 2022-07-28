adc96acfc6bcff730e320c54afb67df107dcbf5f
Annie Chu <chuannie4@gmail.com>
2022-07-28 10:55:10 -0400

    cleaned up functions into classes

--- ctlscraper.py
+++ ctlscraper.py
@@ -4,14 +4,15 @@ import sys
 # adding sonycctl to system path
 sys.path.insert(0, '~/Documents/sonyc/sonycctl')
 import sonycctl
+import numpy as np
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-import numpy as np
+
 """
 || choosing sensor
     - will want to be able to use fqdn instead of deployment_id
 """
-deploy_id = 'deploy-rd4arw' #Input()
+deploy_id = 'deploy-rd4arw' #Input("Enter deployment id...")
 
 """
 || tapping into sonycctl API
@@ -37,7 +38,75 @@ api.login()
 #     pp.pprint(deploymentStatus)
 #     return sensorStatus, deploymentStatus
 
+class SensorHealth:
+    def __init__(self, deploymentID):
+        self.deploymentID = deploymentID
+        deploymentInfo = api.status(self.deploymentID, start='now-30s')
+        self.deployStatus = deploymentInfo[-1]
+    def checkCellUp(self):
+        if self.deployStatus['cell_connected'] and self.deployStatus['ppp0_ip']:
+            print(f"GOOD || Cell Hat USB connected & online")
+        elif self.deployStatus['cell_connected'] and not self.deployStatus['ppp0_ip']:
+            print(f"Error: Cell Hat USB connection detected, but no cell internet connection, is there a sim card?")
+        else:
+            print(f"Error: Cell Hat USB not connected")
 
+        #-- CHECK CELL_STRENGTH:
+        if np.abs(self.deployStatus['cell_strength']) > 85:
+            print(f"GOOD || CELL STRENGTH: {np.abs(self.deployStatus['cell_strength'])}%")
+        else:
+            print(f"Error: Bad cell strength {np.abs(self.deployStatus['cell_strength'])}%")
+
+        # pass
+    def checkIP(self):
+        if self.deployStatus['wlan0_ip']:
+            print(f"GOOD || IP ADDRESS: {self.deployStatus['wlan0_ip']}")
+        else:
+            print(f"Error: No IP ADDRESS")
+        # pass
+    def checkDiskUsage(self):
+        diskVariables = ['tmp_usage','root_usage','varlog_usage']
+        diskLevels = {key: self.deployStatus[key] for key in self.deployStatus.keys() if key in diskVariables}
+        # print(diskLevels)
+        outBounds = [i for i in diskLevels if diskLevels[i] >=15]
+        # print(outBounds)
+        if not outBounds:
+            print('GOOD || Disk Levels: in bounds')
+            for g in diskLevels:
+                pp.pprint(f'{g}:{diskLevels[g]}')
+        else:
+            for x in outBounds:
+                print(f'Error: {x} value out of bounds - {diskLevels[x]}')
+        # pass
+    def checkMicUp(self):
+        if self.deployStatus['mic_connected'] == 1:
+            print('GOOD || Mic: connected')
+        else:
+            print(f"mic bad {self.deployStatus['mic_connected']}")
+
+        #-- CHECK LAEQ LEVELS:
+        if self.deployStatus['laeq'] > 40 and self.deployStatus['laeq'] < 60:
+            print(f"GOOD || LAeq levels -  {self.deployStatus['laeq']}")
+        else:
+            print(f"Error: laeq levels out of bounds - {self.deployStatus['laeq']}")
+
+        # pass
+    def checkAll(self):
+        print(f'---------------- Checking all sensor [{self.deploymentID}] statuses... ----------------')
+        print("==============\nChecking CELL STATUS...\n==============")
+        self.checkCellUp()
+        print("==============\nChecking DISK STATUS...\n==============")
+        self.checkDiskUsage()
+        print("==============\nChecking IP STATUS...\n==============")
+        self.checkIP()
+        print("==============\nChecking MIC STATUS...\n==============")
+        self.checkMicUp()
+
+# testid = SensorHealth(deploy_id) 
+# testid.checkAll()
+
+
+"""
 def sensorCheck(deployment_id):
     status = api.status(deployment_id, start='now-30s')
     s = status[-1]
@@ -89,13 +158,32 @@ def sensorCheck(deployment_id):
     else:
         print(f"Error: laeq levels out of bounds - {s['laeq']}")
 
-sensorCheck(deploy_id)
-
+# sensorCheck(deploy_id)
+"""
 """
 || [DONE] checking deployment info
     - is there a title?
     - is life_stage active?
 """
+class DeploymentHealth:
+    def __init__(self, deploymentID):
+        self.deploymentID = deploymentID
+        targets = ['title', 'deployment_id', 'life_stage', 'fqdn'] #can get rid of fqdn once fqdn replaces deployment_id
+        self.dd = api.deploy.info(self.deploymentID)
+        self.result = {key: self.dd[key] for key in self.dd.keys() if key in targets}
+    def checkValues(self):
+        noValues = [i for i in self.result if not self.result[i]]
+        if not noValues:
+            print('Success: Deployment info looks good')
+            pp.pprint(self.result)
+        else:
+            for error in noValues:
+                print(f'ERROR: no value for - {error}')
+                # raise KeyError(f'ERROR: no value for - {error}')
+
+testid_deploy = DeploymentHealth(deploy_id) 
+testid_deploy.checkValues()
+"""           
 def deploymentCheck(deployment_id):
     dd = api.deploy.info(deployment_id) #deployment data
     targets = ['title', 'deployment_id', 'life_stage', 'fqdn'] #can get rid of fqdn once fqdn replaces deployment_id
@@ -111,6 +199,7 @@ def deploymentCheck(deployment_id):
             # raise KeyError(f'ERROR: no value for - {error}')
 
 # deploymentCheck(deploy_id)
+"""
 
 """
 || check audio data
